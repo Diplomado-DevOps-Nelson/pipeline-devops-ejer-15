cafc2ef8dc8e7bd8552b8a330525508097abb7b9
nelsonagurtop@gmail.com <nelsonagurtop@gmail.com>
2022-01-30 20:04:36 -0400

    actualizo arch maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -13,25 +13,25 @@ def call(String pipelineType){
 
     stage('Compile') {
       STAGE = env.STAGE_NAME
-      bat './mvnw.bat clean compile -e'
+      bat 'mvnw.cmd clean compile -e'
     }
 
     stage('Test') {
       STAGE = env.STAGE_NAME
-      bat './mvnw.bat clean test -e'
+     // bat './mvnw.bat clean test -e'
     }
 
     stage('Jar') {
       STAGE = env.STAGE_NAME
-      bat './mvnw.bat clean package -e'
+     // bat './mvnw.bat clean package -e'
     }
   } else {
     figlet 'Delivery Continuo'
     
     stage('Run') {
       STAGE = env.STAGE_NAME
-      bat 'nohup bash mvnw.bat spring-boot:run &'
-      sleep 20
+     // bat 'nohup bash mvnw.bat spring-boot:run &'
+     // sleep 20
     }
     
    /* stage('TestApp') {
