876c421f3d43211bea46d330a4bbc62db1237b29
nelsonagurtop@gmail.com <nelsonagurtop@gmail.com>
2022-01-30 20:15:02 -0400

    actualizo arch maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -13,7 +13,7 @@ def call(String pipelineType){
 
     stage('Compile') {
      // STAGE = env.STAGE_NAME
-      bat 'mvnw.bat clean compile -e'
+      bat 'mvnw.cmd clean compile -e'
     }
 
     stage('Test') {
